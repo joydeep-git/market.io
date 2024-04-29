4b2719137c214dd9b7ecddc4cc356432b6e4a7ae
Joydeep <joydeep.accounts@tuta.io>
2024-04-29 10:22:26 +0530

    Wrapper component for same width in all pages

---
+++ src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+// PROJECT IMPORT
+import { cn } from "@/lib/utils";
+
+const MaxWidthWrapper = ( { className, children }:
+    {
+      className?: string,
+      children: ReactNode
+    }
+) => {
+  return (
+    <div className={cn("max-w-screen-xl w-full mx-auto px-2.5 md:px-20", className)}>
+      {children}
+    </div>
+  )
+}
+
+export default MaxWidthWrapper;
